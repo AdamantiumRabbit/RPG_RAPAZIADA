1e3b9f47d12898d67e35e8aa34b353d4ab680e07
GoodRabbit <141356782+AdamantiumRabbit@users.noreply.github.com>
2024-04-14 20:52:44 -0300

    inicio
    Um programa que gerencia turnos e fichas para um rpg de mesa

---
+++ Projeto_MorKB/src/main/java/campanha/projeto_morkb/Projeto_MorKB.java
@@ -0,0 +1,13 @@
+package campanha.projeto_morkb;
+
+/**
+ *
+ * @author vini
+ */
+public class Projeto_MorKB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
